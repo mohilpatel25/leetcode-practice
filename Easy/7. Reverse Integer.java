054472327dfc05aa652eef2005f89c74613c82cc
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-01-02 00:07:29 +0530

    Create 7. Reverse Integer.java

---
+++ Easy/7. Reverse Integer.java
@@ -0,0 +1,21 @@
+/*
+Problem Name: 7. Reverse Integer
+Problem Link: https://leetcode.com/problems/reverse-integer/
+*/
+class Solution {
+    public int reverse(int x) {
+        long rev = 0;
+        int sign = 1;
+        if(x < 0){
+            sign = -1;
+            x *= -1;
+        }
+        while(x>0){
+            rev = rev*10 + x%10;
+            x /= 10;
+        }
+        if(rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE)
+                return 0;
+        return (int)rev*sign;
+    }
+}
