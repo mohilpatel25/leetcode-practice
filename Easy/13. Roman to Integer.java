2a0089fd625397f22f8e5ef3b6b13bbf144a22ac
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-01-02 15:18:41 +0530

    Create 13. Roman to Integer.java

---
+++ Easy/13. Roman to Integer.java
@@ -0,0 +1,25 @@
+/*
+Problem Name: 13. Roman to Integer
+Problem Link: https://leetcode.com/problems/roman-to-integer/
+*/
+class Solution {
+    public int romanToInt(String s) {
+        int num = 0;
+        for(;s.length() > 0;){
+            if(s.startsWith("IV"))      {num += 4;      s = s.substring(2);}
+            else if(s.startsWith("V"))  {num += 5;      s = s.substring(1);}
+            else if(s.startsWith("IX")) {num += 9;      s = s.substring(2);}
+            else if(s.startsWith("I"))  {num += 1;      s = s.substring(1);}
+            else if(s.startsWith("XL")) {num += 40;     s = s.substring(2);}
+            else if(s.startsWith("L"))  {num += 50;     s = s.substring(1);}
+            else if(s.startsWith("XC")) {num += 90;     s = s.substring(2);}
+            else if(s.startsWith("X"))  {num += 10;     s = s.substring(1);}
+            else if(s.startsWith("CD")) {num += 400;    s = s.substring(2);}
+            else if(s.startsWith("D"))  {num += 500;    s = s.substring(1);}
+            else if(s.startsWith("CM")) {num += 900;    s = s.substring(2);}
+            else if(s.startsWith("C"))  {num += 100;    s = s.substring(1);}
+            else if(s.startsWith("M"))  {num += 1000;   s = s.substring(1);}
+        }
+        return num;
+    }
+}
