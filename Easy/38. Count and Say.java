7ddb867e68b2a633164e219d40b5f57df2af3aa2
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-01-05 01:36:20 +0530

    Create 38. Count and Say.java

---
+++ Easy/38. Count and Say.java
@@ -0,0 +1,28 @@
+/*
+Problem Name: 38. Count and Say
+Problem Link: https://leetcode.com/problems/count-and-say/
+*/
+class Solution {
+    public String countAndSay(int n) {
+        String say = "1", temp = "";
+        int cnt;
+        char cur;
+        for(int i=2;i<=n;i++){
+            temp = "";
+            cnt = 1;
+            cur = say.charAt(0);
+            for(int j=1;j<say.length();j++){
+                if(cur == say.charAt(j))
+                    cnt++;
+                else{
+                    temp += "" + cnt + cur;
+                    cnt = 1;
+                    cur = say.charAt(j);
+                }
+            }
+            temp += "" + cnt + cur;
+            say = new String(temp);
+        }
+        return say;
+    }
+}
