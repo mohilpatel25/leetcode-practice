9a7de059b7dd0d4bdfafb2d005badd0f4a8945e1
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-01-06 23:21:24 +0530

    Create 49. Group Anagrams.java

---
+++ Medium/49. Group Anagrams.java
@@ -0,0 +1,23 @@
+/*
+Problem Name: 49. Group Anagrams
+Problem Link: https://leetcode.com/problems/group-anagrams/
+*/
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<List<Integer>, List<String>> map = new HashMap<List<Integer>, List<String>>();
+        for(String s: strs){
+            List<Integer> l = new ArrayList<Integer>();
+            for(int i=0;i<26;i++)
+                l.add(0);
+            for(char c:s.toCharArray())
+                l.set(c - 'a', l.get(c - 'a') + 1);
+            if(map.containsKey(l))
+                map.get(l).add(s);
+            else{
+                map.put(l, new ArrayList<String>());
+                map.get(l).add(s);
+            }
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
