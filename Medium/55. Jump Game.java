703c2841bbd935095224a9a3545a540fd9fbaf61
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-01-07 00:24:16 +0530

    Create 55. Jump Game.java

---
+++ Medium/55. Jump Game.java
@@ -0,0 +1,14 @@
+/*
+Problem Name: 55. Jump Game
+Problem Link: https://leetcode.com/problems/jump-game/
+*/
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length, maxIndex = n - 1;
+        for(int i=n-1;i>=0;i--){
+            if(i + nums[i] >= maxIndex)
+                maxIndex = i;
+        }
+        return maxIndex == 0;
+    }
+}
