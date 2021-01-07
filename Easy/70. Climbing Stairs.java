57a1dcd1af94746f88925fbdbcb44d5c4363bddd
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-01-07 23:32:40 +0530

    Create 70. Climbing Stairs.java

---
+++ Easy/70. Climbing Stairs.java
@@ -0,0 +1,19 @@
+/*
+Problem Name: 70. Climbing Stairs
+Problem Link: https://leetcode.com/problems/climbing-stairs/
+*/
+class Solution {
+    public int climbStairs(int n) {
+        int steps[] = new int[3];
+        steps[0] = 1;
+        steps[1] = 2;
+        if(n <= 2)
+            return steps[n-1];
+        for(int i=2;i<n;i++){
+            steps[2] = steps[0] + steps[1];
+            steps[0] = steps[1];
+            steps[1] = steps[2];
+        }
+        return steps[2];
+    }
+}
