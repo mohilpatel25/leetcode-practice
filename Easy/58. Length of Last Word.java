2db4c7513899ad31bb9c6f9b4a527a0c2fc184c8
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-01-07 14:12:53 +0530

    Create 58. Length of Last Word.java

---
+++ Easy/58. Length of Last Word.java
@@ -0,0 +1,15 @@
+/*
+Problem Name: 58. Length of Last Word
+Problem Link: https://leetcode.com/problems/length-of-last-word/
+*/
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int ind = s.length() - 1, end = s.length() - 1;
+        while(ind >= 0 && s.charAt(ind) == ' ')
+            ind--;
+        end = ind;
+        while(ind >= 0 && s.charAt(ind) != ' ')
+            ind--;
+        return end - ind;
+    }
+}
