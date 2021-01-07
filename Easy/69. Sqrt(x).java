182c9550897a484da1070e1aca7b5dff8ef1f5dc
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-01-07 23:26:57 +0530

    Create 69. Sqrt(x).java

---
+++ Easy/69. Sqrt(x).java
@@ -0,0 +1,23 @@
+/*
+Problem Name: 69. Sqrt(x)
+Problem Link: https://leetcode.com/problems/sqrtx/
+*/
+class Solution {
+    public int mySqrt(int x) {
+        if(x <= 1)
+            return x;
+        long l = 0, r = x/2, ans = x;
+        for(;l<=r;){
+            long mid = l + (r-l)/2;
+            if(mid*mid == x)
+                return (int)mid;
+            if(mid*mid > x)
+                r = mid - 1;
+            else{
+                ans = mid;
+                l = mid + 1;
+            }
+        }
+        return (int)ans;
+    }
+}
