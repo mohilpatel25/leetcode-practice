a091ba5fe9a9600598ca250594acc7f9d6375d14
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-01-18 23:49:15 +0530

    Create 175. Combine Two Tables.java

---
+++ Easy/175. Combine Two Tables.java
@@ -0,0 +1,7 @@
+/*
+Problem Name: 175. Combine Two Tables
+Problem Link: https://leetcode.com/problems/combine-two-tables/
+*/
+# Write your MySQL query statement below
+
+SELECT FirstName, LastName, City, State FROM Person LEFT JOIN Address ON Person.PersonId = Address.PersonId;
