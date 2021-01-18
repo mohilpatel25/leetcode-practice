eb43ba3f5fc3b59cd5e7f0d6e7d506318ed933b3
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-01-19 00:04:25 +0530

    Create 183. Customers Who Never Order.sql

---
+++ Easy/183. Customers Who Never Order.sql
@@ -0,0 +1,7 @@
+/*
+Problem Name: 183. Customers Who Never Order
+Problem Link: https://leetcode.com/problems/customers-who-never-order/
+*/
+# Write your MySQL query statement below
+
+SELECT Name AS Customers FROM Customers WHERE Id NOT IN (SELECT CustomerId FROM Orders);
