05509e3e6a5e6813e7aa652ac374f93942d6c3c1
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-02-19 22:51:39 +0530

    Create 205. Isomorphic Strings.java

---
+++ Easy/205. Isomorphic Strings.java
@@ -0,0 +1,23 @@
+/*
+Problem Name: 205. Isomorphic Strings
+Problem Link: https://leetcode.com/problems/isomorphic-strings/
+*/
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            char sch = s.charAt(i), tch = t.charAt(i);
+            if(map.containsKey(sch)){
+                if(map.get(sch) != tch)
+                    return false;
+            }
+            else if(map.values().contains(tch))
+                return false;
+            else{
+                map.put(sch, tch);
+            }
+        }
+        return true;
+    }
+}
