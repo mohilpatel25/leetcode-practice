62a93bd432f0d7e9e47afc2734b48a26553aae78
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-02-19 23:36:05 +0530

    Create 231. Power of Two.java

---
+++ Easy/231. Power of Two.java
@@ -0,0 +1,9 @@
+/*
+Problem Name: 231. Power of Two
+Problem Link: https://leetcode.com/problems/power-of-two/
+*/
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return (n & (n - 1)) == 0 && n > 0;
+    }
+}
