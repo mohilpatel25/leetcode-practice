dbe5474cff1544f61bc141cd2d1f8f972ffca7d6
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-02-19 21:20:03 +0530

    Create 190. Rever.se Bits.java

---
+++ Easy/190. Rever.se Bits.java
@@ -0,0 +1,16 @@
+/*
+Problem Name: 190. Reverse Bits
+Problem Link: https://leetcode.com/problems/reverse-bits/
+*/
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int rev = 0;
+        for(int i=0;i<32;i++){
+            rev = rev << 1;
+            rev += n&1;
+            n >>= 1;
+        }
+        return rev;
+    }
+}
