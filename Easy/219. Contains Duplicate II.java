ac032fe28dad4a68453b0c0bc31a3547b800aee5
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-02-19 23:07:44 +0530

    Create 219. Contains Duplicate II.java

---
+++ Easy/219. Contains Duplicate II.java
@@ -0,0 +1,18 @@
+/*
+Problem Name: 219. Contains Duplicate II
+Problem Link: https://leetcode.com/problems/contains-duplicate-ii/
+*/
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if(map.containsKey(nums[i])){
+                if(i - map.get(nums[i]) <= k)
+                    return true;
+            }
+            map.put(nums[i],i);         
+        }
+        return false;
+    }
+}
