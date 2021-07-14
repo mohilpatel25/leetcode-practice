712a5164bb0f0859f5fb6ec3213b4b5baeac509b
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-07-14 08:41:09 +0530

    Create 401. Binary Watch.java

---
+++ Easy/401. Binary Watch.java
@@ -0,0 +1,26 @@
+/*
+Problem Name: 401. Binary Watch
+Problem Link: https://leetcode.com/problems/binary-watch/
+*/
+/**
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> list = new ArrayList<String>();
+        for(int i=0;i<12;i++){
+            for(int j=0;j<60;j++){
+                if(setBits(i) + setBits(j) == turnedOn){
+                    list.add(j<10 ? i+":0"+j : i+":"+j);
+                }
+            }
+        }
+        return list;
+    }
+    static int setBits(int n){
+        int bits = 0;
+        while(n>0){
+            bits += n&1;
+            n >>= 1;
+        }
+        return bits;
+    }
+}
