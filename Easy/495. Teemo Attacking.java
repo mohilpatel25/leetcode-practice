63dc37ef72fa1d282e17dee56c6839ffba13cb8c
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-07-15 08:13:37 +0530

    Create 495. Teemo Attacking.java

---
+++ Easy/495. Teemo Attacking.java
@@ -0,0 +1,17 @@
+/*
+Problem Name: 495. Teemo Attacking
+Problem Link: https://leetcode.com/problems/teemo-attacking/
+*/
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int seconds = 0, n = timeSeries.length;
+        for(int i=1;i<n;i++){
+            if(timeSeries[i] - timeSeries[i-1] > duration - 1)
+                seconds += duration;
+            else
+                seconds += timeSeries[i] - timeSeries[i-1];
+        }
+        seconds += duration;
+        return seconds;
+    }
+}
