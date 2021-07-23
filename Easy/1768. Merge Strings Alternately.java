e31b636575b4eebcab355ed37ce3bb7a7cae2c84
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-07-23 20:20:18 +0530

    Update 1768. Merge Strings Alternately.java

--- Easy/1768. Merge Strings Alternately.java
+++ Easy/1768. Merge Strings Alternately.java
@@ -4,23 +4,14 @@ Problem Link: https://leetcode.com/problems/merge-strings-alternately/
 */
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int l1 = word1.length(), l2 = word2.length(), i, j;
-        char arr1[] = word1.toCharArray(), arr2[] = word2.toCharArray();
-        String ans = "";
-        for(i=0, j=0;i<l1 && j<l2;){
-            if((i+j)%2 == 0){
-                ans += arr1[i];
-                i++;
-            }
-            else{
-                ans += arr2[j];
-                j++;
-            }
+        int l1 = word1.length(), l2 = word2.length(), i = 0, j = 0;
+        StringBuilder ans = new StringBuilder();
+        for(;i<l1 || j<l2;){
+            if(i < l1)
+                ans.append(word1.charAt(i++));
+            if(j < l2)
+                ans.append(word2.charAt(j++));
         }
-        for(;i<l1;i++)
-            ans += arr1[i];
-        for(;j<l2;j++)
-            ans += arr2[j];
-        return ans;
+        return ans.toString();
     }
 }
