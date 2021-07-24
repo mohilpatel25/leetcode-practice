5cfcfe13a4669f4e925aea648a5bff58dd42ec11
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-07-24 10:23:43 +0530

    Create 1844. Replace All Digits with Characters.java

---
+++ Easy/1844. Replace All Digits with Characters.java
@@ -0,0 +1,20 @@
+/*
+Problem Name: 1844. Replace All Digits with Characters
+Problem Link: https://leetcode.com/problems/replace-all-digits-with-characters/
+*/
+class Solution {
+    public String replaceDigits(String s) {
+        int n = s.length();
+        StringBuilder shifted = new StringBuilder();
+        for(int i=0;i<n;i++){
+            if(i%2 == 0)
+                shifted.append(s.charAt(i));
+            else
+                shifted.append(shift(s.charAt(i-1), s.charAt(i) - '0'));
+        }
+        return shifted.toString();
+    }
+    static char shift(char c, int x){
+        return (char)(((c - 'a') + x%26)%26 + 'a');
+    }
+}
