fd84bc3659c31003d18e643aa7cd402bb0335435
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-07-24 17:47:08 +0530

    Create 1897. Redistribute Characters to Make All Strings Equal.java

---
+++ Easy/1897. Redistribute Characters to Make All Strings Equal.java
@@ -0,0 +1,19 @@
+/*
+Problem Name: 1897. Redistribute Characters to Make All Strings Equal
+Problem Link: https://leetcode.com/problems/redistribute-characters-to-make-all-strings-equal/
+*/
+class Solution {
+    public boolean makeEqual(String[] words) {
+        int n = words.length;
+        int count[] = new int[26];
+        for(int i=0;i<n;i++){
+            for(char c: words[i].toCharArray())
+                count[c - 'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(count[i]%n != 0)
+                return false;
+        }
+        return true;
+    }
+}
