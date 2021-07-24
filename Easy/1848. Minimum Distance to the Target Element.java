618f4ba5c21470a8119b5413274d441e216a95e7
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-07-24 10:32:40 +0530

    Create 1848. Minimum Distance to the Target Element.java

---
+++ Easy/1848. Minimum Distance to the Target Element.java
@@ -0,0 +1,14 @@
+/*
+Problem Name: 1848. Minimum Distance to the Target Element
+Problem Link: https://leetcode.com/problems/minimum-distance-to-the-target-element/
+*/
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int minDist = Integer.MAX_VALUE, n = nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i] == target)
+                minDist = Math.min(minDist, Math.abs(i - start));
+        }
+        return minDist;
+    }
+}
