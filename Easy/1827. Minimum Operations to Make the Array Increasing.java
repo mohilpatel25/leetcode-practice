91b34861994c2b1b37aeb0df5fd775442d3b7810
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-07-24 09:58:45 +0530

    Create 1827. Minimum Operations to Make the Array Increasing.java

---
+++ Easy/1827. Minimum Operations to Make the Array Increasing.java
@@ -0,0 +1,16 @@
+/*
+Problem Name: 1827. Minimum Operations to Make the Array Increasing
+Problem Link: https://leetcode.com/problems/minimum-operations-to-make-the-array-increasing/
+*/
+class Solution {
+    public int minOperations(int[] nums) {
+        int operations = 0, n = nums.length, diff = 0;
+        for(int i=1;i<n;i++){
+            if(nums[i-1] >= nums[i]){
+                operations += nums[i-1] - nums[i] + 1;
+                nums[i] = nums[i-1] + 1;                
+            }
+        }
+        return operations;
+    }
+}
