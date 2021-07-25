1d2d9f2f9f4b00ba7bd64f8bd56b17396cae0557
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-07-25 19:56:04 +0530

    Create 807. Max Increase to Keep City Skyline.java

---
+++ Medium/807. Max Increase to Keep City Skyline.java
@@ -0,0 +1,31 @@
+/*
+Problem Name: 807. Max Increase to Keep City Skyline
+Problem Link: https://leetcode.com/problems/max-increase-to-keep-city-skyline/
+*/
+class Solution {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int rows = grid.length, cols = grid[0].length;
+        int rowLine[] = new int[rows], colLine[] = new int[cols];
+        for(int i=0;i<rows;i++){
+            int max = 0;
+            for(int j=0;j<cols;j++){
+                max = Math.max(max, grid[i][j]);
+            }
+            rowLine[i] = max;
+        }
+        for(int i=0;i<cols;i++){
+            int max = 0;
+            for(int j=0;j<rows;j++){
+                max = Math.max(max, grid[j][i]);
+            }
+            colLine[i] = max;
+        }
+        int increase = 0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                increase += Math.min(rowLine[i], colLine[j]) - grid[i][j];
+            }
+        }
+        return increase;
+    }
+}
