08f9cdf193d340a042c9e463a857d54d6932fda1
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-07-25 10:42:06 +0530

    Create 195. Tenth Line.sh

---
+++ Easy/195. Tenth Line.sh
@@ -0,0 +1,14 @@
+# Problem Name: 195. Tenth Line
+# Problem Link: https://leetcode.com/problems/tenth-line/
+
+# Read from the file file.txt and output the tenth line to stdout.
+input="file.txt"
+i=10
+while IFS= read -r line
+do
+    i=$( expr $i - 1 )
+    if [ $i -eq 0 ]
+    then
+      echo $line
+    fi
+done < "$input"
