0431b3a449f47491fe3b4382640822e632388a3e
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-07-25 20:01:22 +0530

    Create 1315. Sum of Nodes with Even-Valued Grandparent.java

---
+++ Medium/1315. Sum of Nodes with Even-Valued Grandparent.java
@@ -0,0 +1,34 @@
+/*
+Problem Name: 1315. Sum of Nodes with Even-Valued Grandparent
+Problem Link: https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumEvenGrandparent(TreeNode root) {
+        return sumGP(root, new TreeNode(1), new TreeNode(1));
+    }
+    static int sumGP(TreeNode root, TreeNode gp, TreeNode p){
+        if(root == null)
+            return 0;
+        int sum = 0;
+        if(gp.val%2 == 0)
+            sum += root.val;
+        sum += sumGP(root.left, p, root);
+        sum += sumGP(root.right, p, root);
+        return sum;
+    }
+}
