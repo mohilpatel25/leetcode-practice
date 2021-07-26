0c0da5aa720e2bdd4c5671f5e3e80420057dc58a
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-07-26 09:04:31 +0530

    Create 1409. Queries on a Permutation With Key.java

---
+++ Medium/1409. Queries on a Permutation With Key.java
@@ -0,0 +1,18 @@
+/*
+Problem Name: 1409. Queries on a Permutation With Key
+Problem Link: https://leetcode.com/problems/queries-on-a-permutation-with-key/
+*/
+class Solution {
+    public int[] processQueries(int[] queries, int m) {
+        List<Integer> permutation = new ArrayList<Integer>();
+        for(int i=1;i<=m;i++)
+            permutation.add(i);
+        int n = queries.length, answer[] = new int[n];
+        for(int i=0;i<n;i++){
+            answer[i] = permutation.indexOf(queries[i]);
+            permutation.remove(answer[i]);
+            permutation.add(0, queries[i]);
+        }
+        return answer;
+    }
+}
