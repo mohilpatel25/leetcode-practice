435ed2a159e829e7111044ea77d46ffab33e2385
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-07-26 09:32:57 +0530

    Create 1551. Minimum Operations to Make Array Equal.java

---
+++ Medium/1551. Minimum Operations to Make Array Equal.java
@@ -0,0 +1,9 @@
+/*
+Problem Name: 1551. Minimum Operations to Make Array Equal
+Problem Link: https://leetcode.com/problems/minimum-operations-to-make-array-equal/
+*/
+class Solution {
+    public int minOperations(int n) {
+        return (n%2 == 0) ? (n/2)*(4 + (n/2-1)*4)/4 : (n/2)*(8 + (n/2-1)*4)/4;
+    }
+}
