745a1974e43d96d56cba497ab7c200f0291485e2
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-08-02 08:59:28 +0530

    Create 1910. Remove All Occurrences of a Substring.java

---
+++ Medium/1910. Remove All Occurrences of a Substring.java
@@ -0,0 +1,37 @@
+/*
+Problem Name: 1910. Remove All Occurrences of a Substring
+Problem Link: https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
+*/
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        Stack<Character> stck = new Stack<Character>();
+        int slen = s.length(), plen = part.length();
+        if(slen < plen)
+            return s;
+        for(char c: s.toCharArray()){
+            stck.push(c);
+            if(c == part.charAt(plen - 1))
+                remove(stck, part);
+            if(!stck.isEmpty() && stck.peek() == part.charAt(plen - 1))
+                remove(stck, part);
+        }
+        String answer = "";
+        while(!stck.isEmpty())
+            answer = stck.pop() + answer;
+        return answer.toString();
+    }
+    static void remove(Stack<Character> stck, String part){
+        int len = part.length();
+        Stack<Character> temp = new Stack<Character>();
+        if(stck.size() < len)
+            return;
+        for(int i=len-1;i>=0;i--){
+            if(stck.peek() != part.charAt(i)){
+                while(!temp.isEmpty())
+                    stck.push(temp.pop());
+                return;
+            }
+            temp.push(stck.pop());
+        }
+    }
+}
