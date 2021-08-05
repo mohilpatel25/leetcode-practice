621dfc3941606035f4bde2f594ece1eaa8899fa6
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-08-05 22:15:48 +0530

    Create 1833. Maximum Ice Cream Bars.java

---
+++ Medium/1833. Maximum Ice Cream Bars.java
@@ -0,0 +1,20 @@
+/*
+Problem Name: 1833. Maximum Ice Cream Bars
+Problem Link: https://leetcode.com/problems/maximum-ice-cream-bars/
+*/
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        int count[] = new int[100001];
+        int bars = 0, n = costs.length;
+        for(int i=0;i<n;i++)
+            count[costs[i]]++;
+        for(int i=1;i<=100000;i++){
+            if(count[i] > 0){
+                int quant = Math.min(coins/i, count[i]);
+                coins -= quant*i;
+                bars += quant;
+            }
+        }
+        return bars;
+    }
+}
