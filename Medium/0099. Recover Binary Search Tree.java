faa0077e39259a2efb26b4db3119dea73265a30e
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-08-05 08:29:45 +0530

    Create 0099. Recover Binary Search Tree.java

---
+++ Medium/0099. Recover Binary Search Tree.java
@@ -0,0 +1,39 @@
+/*
+Problem Name: 99. Recover Binary Search Tree
+Problem Link: https://leetcode.com/problems/recover-binary-search-tree/
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode previous = new TreeNode(Integer.MIN_VALUE), first = null, second = null;
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+        int t = first.val;
+        first.val = second.val;
+        second.val = t;
+    }
+    void inorder(TreeNode root){
+        if(root == null)
+            return;
+        inorder(root.left);
+        if((first == null) && (previous.val > root.val))
+            first = previous;
+        if((first != null) && (previous.val > root.val))
+            second = root;
+        previous = root;
+        inorder(root.right);
+    }
+}
