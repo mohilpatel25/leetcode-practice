b5f0f69cd65d80cdc626c5df0b932037c5c37d8b
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-08-05 13:11:30 +0530

    Update 0812. Largest Triangle Area.java

--- Easy/0812. Largest Triangle Area.java
+++ Easy/0812. Largest Triangle Area.java
@@ -15,7 +15,7 @@ class Solution {
         }
         return area;
     }
-    static double calcArea(int[] p1, int[] p2, int[] p3){
+    double calcArea(int[] p1, int[] p2, int[] p3){
         return Math.abs((p1[0]*(p2[1]-p3[1]) + p2[0]*(p3[1] - p1[1]) + p3[0]*(p1[1] - p2[1]))/2.0);
     }
 }
