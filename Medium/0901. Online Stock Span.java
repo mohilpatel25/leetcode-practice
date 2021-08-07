6517df80e95dd2223202bd4f665976c6fe71f350
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-08-07 12:59:44 +0530

    Create 0901. Online Stock Span.java

---
+++ Medium/0901. Online Stock Span.java
@@ -0,0 +1,25 @@
+/*
+Problem Name: 901. Online Stock Span
+Problem Link: https://leetcode.com/problems/online-stock-span/
+*/
+class StockSpanner {
+    Stack<Pair<Integer,Integer>> stck;
+    public StockSpanner() {
+        stck = new Stack<Pair<Integer,Integer>>();
+    }
+    
+    public int next(int price) {
+        int span = 0;
+        while(!stck.isEmpty() && stck.peek().getKey() <= price){
+            span += stck.pop().getValue();
+        }
+        stck.push(new Pair(price, span + 1));
+        return span + 1;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
