404473d37c2ffb9bc4bc30d8336f0e106cc81470
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2021-08-09 10:31:01 +0530

    Create 0542. 01 Matrix.java

---
+++ Medium/0542. 01 Matrix.java
@@ -0,0 +1,41 @@
+/*
+Problem Name: 542. 01 Matrix
+Problem Link: https://leetcode.com/problems/01-matrix/
+*/
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int rows = mat.length, cols = mat[0].length;
+        int distance[][] = new int[rows][cols];
+        for(int[] r: distance)
+            Arrays.fill(r, 100000);
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(mat[i][j] == 0)
+                    distance[i][j] = 0;
+                else{
+                    if(i > 0)
+                        distance[i][j] = Math.min(distance[i][j], distance[i-1][j] + 1);
+                    if(j > 0)
+                        distance[i][j] = Math.min(distance[i][j], distance[i][j-1] + 1);
+                }
+            }
+        }
+        for(int i=rows-1;i>=0;i--){
+            for(int j=cols-1;j>=0;j--){
+                if(i < rows-1)
+                    distance[i][j] = Math.min(distance[i][j], distance[i+1][j] + 1);
+                if(j < cols-1)
+                    distance[i][j] = Math.min(distance[i][j], distance[i][j+1] + 1);
+            }
+        }
+        return distance;
+    }
+    void print(int arr[][]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++)
+                System.out.print(arr[i][j]+" ");
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
