3e355ba13c4bd63f7ab9ee26b210fee4029459f9
Mohil Patel <48318085+mohilpatel25@users.noreply.github.com>
2022-01-30 22:09:31 +0530

    Update updateReadme.py

--- updateReadme.py
+++ updateReadme.py
@@ -11,15 +11,13 @@ The questions have been coded using Java. <br>
 
 '''.format(len(os.listdir('Easy')), len(os.listdir('Medium')), len(os.listdir('Hard')))
 
-readme += '''|#|Problem Name|Difficulty|
-|:---:|---|:---:|
-'''
+readme += '### Problems\n\n'
 
 dirs = ['Easy', 'Medium', 'Hard']
 color = {
-	'Easy': 'https://img.shields.io/badge/-Easy-%230b915e',
-	'Medium': 'https://img.shields.io/badge/-Medium-%23e38a29',
-	'Hard': 'https://img.shields.io/badge/-Hard-%23e0375b',
+	'Easy': 'https://img.shields.io/badge/-{}-%230b915e',
+	'Medium': 'https://img.shields.io/badge/-{}-%23e38a29',
+	'Hard': 'https://img.shields.io/badge/-{}-%23e0375b',
 }
 files = {}
 for dir in dirs:
@@ -27,10 +25,12 @@ for dir in dirs:
 		files[file] = dir
 
 for file in sorted(files.keys()):
-		splits = file.split('.')
-		link = files[file]+"/"+file
-		link = link.replace(" ", "%20")
-		readme = readme + '|{}|[{}]({})|![]({})|\n'.format(splits[0].lstrip('0'), splits[1][1:], link, color[files[file]])
-		
+	splits = file.replace('.md', '').lstrip('0')
+	link = files[file]+"/"+file
+	link = link.replace(" ", "%20")
+	badge = color[files[file]].format(splits)
+	badge = badge.replace(" ", "%20")
+	readme = readme + f'<a href="{link}">![]({badge})</a>\n'
+	
 with open('README.md', 'w') as f:
 	f.write(readme)
